f9289a09c4fda7bfaa8361ef8a4cb5e20ac7e541
magdalena-zydorczak <s253712@student.dtu.dk>
2025-11-29 18:03:37 +0100

    Code cleanup

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -2,17 +2,20 @@ library("tidyverse")
 
 CPM_normalization <- function(raw_counts){
   CPM_normalized_counts <- raw_counts |>
-    mutate(across(!GeneFeature, ~(.*1000000)/sum(.)))
+    mutate(across(!GeneFeature,
+                  ~(.*1000000)/sum(.)))
 
   return(CPM_normalized_counts)
 }
 
 quantile_normalization <- function(raw_counts){
   raw_counts_ranks <- raw_counts |>
-    mutate(across(!GeneFeature, ~rank(., ties.method = "min")))
+    mutate(across(!GeneFeature,
+                  ~rank(., ties.method = "min")))
 
   raw_counts_sorted_cols <- raw_counts |>
-    mutate(across(!GeneFeature, sort))
+    mutate(across(!GeneFeature,
+                  sort))
 
   raw_counts_means <- raw_counts_sorted_cols |>
     rowwise() |>
@@ -20,9 +23,13 @@ quantile_normalization <- function(raw_counts){
     ungroup() |>
     rowid_to_column("ID")
 
-  lookup_vec <- setNames(raw_counts_means$m, raw_counts_means$ID)
+  lookup_vec <- setNames(raw_counts_means$m,
+                         raw_counts_means$ID)
+
   quantile_normalized_counts <- raw_counts_ranks |>
-    mutate(across(!GeneFeature, ~ lookup_vec[.] |> as.numeric()))
+    mutate(across(!GeneFeature,
+                  ~ lookup_vec[.] |>
+                      as.numeric()))
 
   return(quantile_normalized_counts)
 }
@@ -36,7 +43,8 @@ GEP_score_calculation <- function(quantile_normalized_counts, metadata){
   )
 
   augmented_metadata <- quantile_normalized_counts |>
-    mutate(across(!GeneFeature, ~log10(.+1))) |>
+    mutate(across(!GeneFeature,
+                  ~log10(.+1))) |>
     right_join(signature_genes,
                join_by(GeneFeature == gene)) |>
     summarise(across(!GeneFeature, mean)) |>
@@ -52,7 +60,8 @@ GEP_score_calculation <- function(quantile_normalized_counts, metadata){
 run_fisher_test <- function(data, var1, var2) {
   # var1, var2: column names as strings
 
-  tab <- table(data[[var1]], data[[var2]])
+  tab <- table(data[[var1]],
+               data[[var2]])
   fisher.test(tab)
 }
 
